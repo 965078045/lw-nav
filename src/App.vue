78b15fb710086f02cb41ae1dd3f59fb50da5d6f6
965078045 <965078045@qq.com>
2025-12-24 10:15:52 +0800

    Wrap RouterView in a main container div

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <RouterView />
+  <div id="app">
+    <div class="main-container">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style scoped>
